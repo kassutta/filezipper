6b7df5be0ac4faf4e4b1b900f5947c95c3ab0ed4
Mihir Singh <42788075+kassutta@users.noreply.github.com>
2021-02-02 19:02:23 +0530

    Update heap.js

--- heap.js
+++ heap.js
@@ -18,7 +18,20 @@ class BinaryHeap {
     empty(){
         return ( this.size()===0 );
     }
+    bubbleUp() {
+        let index = this.size() - 1;
 
+        while (index > 0) {
+            let element = this.heap[index],
+                parentIndex = Math.floor((index - 1) / 2),
+                parent = this.heap[parentIndex];
+
+            if (parent[0] >= element[0]) break;
+            this.heap[index] = parent;
+            this.heap[parentIndex] = element;
+            index = parentIndex
+        }
+    }
     extractMax() {
         const max = this.heap[0];
         const tmp = this.heap.pop();
@@ -28,7 +41,7 @@ class BinaryHeap {
         }
         return max;
     }
-
+    
     sinkDown(index) {
 
         let left = 2 * index + 1,
